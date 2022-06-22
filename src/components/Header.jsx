9a5b7c2bdbed8c144f016db0c2bee954e90156f7
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-06-22 11:24:35 -0300

    header component finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import styles from "./Header.module.css";
 
+import IgniteLogo from "../assets/ignite-logo.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={IgniteLogo} alt="Logotipo do Ignite" />
+      <span>Ignite Feed</span>
     </header>
   );
 }
