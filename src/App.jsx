0c041049d9fc4b56f7dec81a119afcbd26c03ada
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-06-27 09:21:39 -0300

    Posts List

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,58 @@ import { Post } from "./components/Post";
 import "./styles/global.css";
 import styles from "./styles/App.module.css";
 
+// Posts
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/eduardoaguiars.png",
+      name: "Eduardo Aguiar",
+      role: "Front-end Developer"
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala Galera!"
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu Github. É um projeto que fiz no NLW."
+      },
+      {
+        type: "link",
+        content: "https://github.com/eduardoaguiars"
+      }
+    ],
+    publishedAt: new Date("2022-06-25 08:13:30")
+  }, // Post 1
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @ Rocketseat"
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala Galera!"
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu Github. É um projeto que fiz no NLW."
+      },
+      {
+        type: "link",
+        content: "https://github.com/diego3g"
+      }
+    ],
+    publishedAt: new Date("2022-06-27 10:13:30")
+  } // Post 2
+];
+
 // App Function Component
 export function App() {
   return (
