bf9d51afdfba08ee3d3e91f832e08377189f19a8
Eduardo Aguiar <edu.pessoal@outlook.com>
2022-06-27 10:08:39 -0300

    if to switch

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -41,14 +41,15 @@ export function Post({ author, publishedAt, content }) {
       {/* Post Content */}
       <div className={styles.content}>
         {content.map(line => {
-          if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
-          } else if (line.type === "link") {
-            return (
-              <p>
-                <a href={line.content}>{line.content}</a>
-              </p>
-            );
+          switch (line.type) {
+            case "paragraph":
+              return <p>{line.content}</p>;
+            case "link":
+              return (
+                <p>
+                  <a href={line.content}>{line.content}</a>
+                </p>
+              );
           }
         })}
       </div>
